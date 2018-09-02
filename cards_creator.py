e2871b7753a614f0bfa3db2b12078ac43438b34e
gui <guilherme.castro@fatec.sp.gov.br>
2018-09-02 13:39:18 -0300

    Version 1.0

---
+++ cards_creator.py
@@ -0,0 +1,30 @@
+from random import randint
+scenarios = []
+
+hue = ""
+for i in range(50):
+    operations = ['+', '-', '*', '%']
+    a = randint(1, 100)
+    b = randint(1, 100)
+    operand = randint(0, 2)
+    answer = str(a)+operations[operand]+str(b)
+    answer = eval(answer)
+
+    a = { 'xyzhackerCardxyz' : {
+        'xyzdescriptionxyz' : "{} {} {} equals to: ".format(a,operations[operand],b),
+        'xyzpowerxyz' : 4,},
+        'xyzplayerCardsxyz' : [{
+        'xyzdescriptionxyz' : "{}".format(answer),
+        'xyzpowerxyz' : 5,},
+      { 'xyzdescriptionxyz' : "{}".format(answer+randint(2, 13)),
+        'xyzpowerxyz' : 3,},
+      { 'xyzdescriptionxyz' : "{}".format(answer+randint(2, 27)),
+        'xyzpowerxyz' : 1,}]}
+    scenarios.append(a)
+    # print(a)
+    hue += str(a)
+
+hue = hue.replace("'xyz", "")
+hue = hue.replace("xyz'", "")
+hue = hue.replace("}]}", "}]},\n")
+print(hue)
